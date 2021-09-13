88f1993ea0794fe69f4a353d97c0974c63055613
garold664 <garold664@gmail.com>
2021-09-13 18:01:06 +0300

    дз: Posts берёт данные из объекта

--- src/components/profile/posts/Posts.jsx
+++ src/components/profile/posts/Posts.jsx
@@ -3,15 +3,27 @@ import Post from './post/Post'
 // import ava from "../../img/elon.jpg"
 import ava_min from "../../../img/elon_min.jpg"
 
+let postItems = [
+    {name: 'some guy', message: "Any message"},
+    {name: 'Elon Musk', message: "Lorem Ipsum"},
+    {name: 'Elon Musk', message: "Anything else"},
+    {name: 'Elon Musk', message: "Elon Musk is really great inventor and entrepreneur"},
+];
+
 function Posts() {
     return (
         <div className='posts'>
             <input placeholder='enter the post' />
             <button>Add post</button>
-            <Post name="some guy"  message = "Any message" />
+            {
+                postItems.map((e) => {
+                    return <Post name = {e.name} message = {e.message} />
+                })
+            }
+            {/* <Post name="some guy"  message = "Any message" />
             <Post name="Elon Musk" message = "Lorem Ipsum" />
             <Post name="Elon Musk" message = "Anything else" />
-            <Post name="Elon Musk" message = "Elon Musk is really great inventor and entrepreneur" />
+            <Post name="Elon Musk" message = "Elon Musk is really great inventor and entrepreneur" /> */}
         </div>
   );
 }
