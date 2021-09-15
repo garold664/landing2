790356f326aa6be394e09bd861e20272bb60318a
garold664 <garold664@gmail.com>
2021-09-15 08:57:46 +0300

    добавляем вывод в alert содержимое инпута при нажатии на кнопку

--- src/components/profile/posts/Posts.jsx
+++ src/components/profile/posts/Posts.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import Post from './post/Post'
-// import ava from "../../img/elon.jpg"
-import ava_min from "../../../img/elon_min.jpg"
+
+let postText = React.createRef()
+
+let addPost = () => {
+    alert(postText.current.value)
+}
 
 function Posts(props) {
     return (
         <div className='posts'>
-            <input placeholder='enter the post' />
-            <button>Add post</button>
+            <input ref = {postText} placeholder='enter the post' />
+            <button onClick={addPost}>Add post</button>
             {
                 props.postItems.map((e) => {
                     return <Post name = {e.name} message = {e.message} likes = {e.likes} />
                 })
             }
-            {/* <Post name="some guy"  message = "Any message" />
-            <Post name="Elon Musk" message = "Lorem Ipsum" />
-            <Post name="Elon Musk" message = "Anything else" />
-            <Post name="Elon Musk" message = "Elon Musk is really great inventor and entrepreneur" /> */}
+            
         </div>
   );
 }
