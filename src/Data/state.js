abb9d15a597e2cb2857db5ab9db6c76c30523276
garold664 <garold664@gmail.com>
2021-09-19 20:46:19 +0300

    в state.js всё упаковали в объект store, но импорт пока не исправили

--- src/Data/state.js
+++ src/Data/state.js
@@ -1,79 +1,81 @@
-let state = {
-    navbar: {
-        friends: [
-            {name: 'Bill', ava: 'friend1.jpg'},
-            {name: 'John', ava: 'friend2.jpg'},
-            {name: 'Alex', ava: 'friend3.png'},
-        ]
+
+let store = {
+    _state = {
+        navbar: {
+            friends: [
+                {name: 'Bill', ava: 'friend1.jpg'},
+                {name: 'John', ava: 'friend2.jpg'},
+                {name: 'Alex', ava: 'friend3.png'},
+            ]
+        },
+        profilePage: {
+            postItems: [
+                {name: 'some guy', message: "Any message", likes: 1},
+                {name: 'Elon Musk', message: "Lorem Ipsum", likes: 3},
+                {name: 'Elon Musk', message: "Anything else", likes: 2},
+                {name: 'Elon Musk', message: "Elon Musk is really great inventor and entrepreneur", likes: 15},
+            ],
+            newPostText: ''
+        },
+
+        dialogsPage: {
+            dialogNames: [
+                {name: 'Иван Иванов', id: 1},
+                {name: 'Илон Маск', id: 2},
+                {name: 'Билл Гейтс', id: 3}
+            ],  
+
+        
+            messageItems: [
+                {message: 'Привет', id: 1},
+                {message: 'Го на Марс', id: 2},
+                {message: 'Где мой чип?', id: 3}
+            ],
+            newMessageText: ''
+        }
     },
-    profilePage: {
-        postItems: [
-            {name: 'some guy', message: "Any message", likes: 1},
-            {name: 'Elon Musk', message: "Lorem Ipsum", likes: 3},
-            {name: 'Elon Musk', message: "Anything else", likes: 2},
-            {name: 'Elon Musk', message: "Elon Musk is really great inventor and entrepreneur", likes: 15},
-        ],
-        newPostText: ''
+
+    addPost (postText) {
+        let newPost = {
+            message: postText,
+            id: 4,
+            likes: 0 
+        }
+
+        state.profilePage.postItems.unshift(newPost)
+        state.profilePage.newPostText = ''
+        rerenderTree(state)
+        // console.log(state)
     },
 
-    dialogsPage: {
-        dialogNames: [
-            {name: 'Иван Иванов', id: 1},
-            {name: 'Илон Маск', id: 2},
-            {name: 'Билл Гейтс', id: 3}
-        ],  
+    addMessage (messageText) {
+        let newMessage = {
+            message: messageText,
+            id: 4,
+        }
 
-      
-        messageItems: [
-            {message: 'Привет', id: 1},
-            {message: 'Го на Марс', id: 2},
-            {message: 'Где мой чип?', id: 3}
-        ],
-        newMessageText: ''
+        state.dialogsPage.messageItems.unshift(newMessage)
+        state.dialogsPage.newMessageText = '';
+        rerenderTree(state)
+        // console.log(state)
+    },
+
+    onPostChange (text) {
+        state.profilePage.newPostText = text
+        rerenderTree(state)
+    },
+
+    onMessageChange (text) {
+        state.dialogsPage.newMessageText = text
+        rerenderTree(state)
+    },
+
+    rerenderTree () {
+        console.log("It's fake")
+    },
+
+    subscribe (observer) {
+        rerenderTree = observer
     }
-};
-
-export let addPost = (postText) => {
-    let newPost = {
-        message: postText,
-        id: 4,
-        likes: 0 
-    }
-
-    state.profilePage.postItems.unshift(newPost)
-    state.profilePage.newPostText = ''
-    rerenderTree(state)
-    // console.log(state)
 }
-
-export let addMessage = (messageText) => {
-    let newMessage = {
-        message: messageText,
-        id: 4,
-    }
-
-    state.dialogsPage.messageItems.unshift(newMessage)
-    state.dialogsPage.newMessageText = '';
-    rerenderTree(state)
-    // console.log(state)
-}
-
-export let onPostChange = (text) => {
-    state.profilePage.newPostText = text
-    rerenderTree(state)
-}
-
-export let onMessageChange = (text) => {
-    state.dialogsPage.newMessageText = text
-    rerenderTree(state)
-}
-
-let rerenderTree = () => {
-    console.log("It's fake")
-}
-
-export let subscribe = (observer) => {
-    rerenderTree = observer
-}
-
-export default state;
+export default store;
